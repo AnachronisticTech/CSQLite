99a351587b2444cfb6db094ad69a5ae1a591ae17
Daniel <danielmarriner@anachronistic-tech.co.uk>
2021-04-30 20:00:27 +0300

    Hidden unnecessary products and targets

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftQLite",
+    name: "csqlite",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "csqlite", targets: ["csqlite"]),
-        .library(name: "SwiftQLite", targets: ["SwiftQLite"]),
-        .executable(name: "test", targets: ["test"])
+        .library(name: "CSQLite", targets: ["CSQLite"]),
+        // .library(name: "SwiftQLite", targets: ["SwiftQLite"]),
+        // .executable(name: "test", targets: ["test"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "csqlite"),
-        .target(name: "SwiftQLite", dependencies: ["csqlite"]),
-        .executableTarget(name: "test", dependencies: ["SwiftQLite"])
+        .target(name: "CSQLite"),
+        // .target(name: "SwiftQLite", dependencies: ["CSQLite"]),
+        // .executableTarget(name: "test", dependencies: ["SwiftQLite"])
     ]
 )
--- Sources/SwiftQLite/SwiftQLite.swift
+++ Sources/SwiftQLite/SwiftQLite.swift
@@ -1,2 +1,2 @@
-import csqlite
+import CSQLite
 
